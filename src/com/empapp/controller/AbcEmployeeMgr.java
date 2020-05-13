2165f54688723f36823218daa2006d1c74357f37
Rahul <rahulbapumore@gmail.com>
2020-05-13 20:56:19 +0530

    Controller class added

---
+++ src/com/empapp/controller/AbcEmployeeMgr.java
@@ -0,0 +1,49 @@
+package com.empapp.controller;
+import com.empapp.model.Employee;
+
+public class AbcEmployeeMgr implements IEmployeeManager
+{
+   public static final int workingHours = 8;
+   public static final int hourlyWage = 30;
+
+ 
+  @Override
+  public int calculateDailyWage(Employee emp)
+  {
+        if(emp.getIsPresent() == true)
+        {
+            if(emp.getDoneParttime() == true)
+            {
+                return (hourlyWage*workingHours + hourlyWage*workingHours );
+            }
+            else
+            {
+                return (hourlyWage*workingHours );
+            }
+        }
+	return 0;
+  }
+  
+
+  @Override
+  public int calculateMonthlyWage(Employee em)
+  {
+
+        if(em.getIsPresent() == true)
+        {
+            if(em.getDoneParttime() == true)
+            {
+                    return ((hourlyWage*workingHours + hourlyWage*workingHours)*hourlyWage);
+            }
+            else
+            {
+                    return (hourlyWage*workingHours*hourlyWage);
+            }
+        }
+	return 0;
+
+  }
+
+}
+
+
